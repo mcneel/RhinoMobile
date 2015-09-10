f03e752960f012aa8960bec04ff620f96ef9f7fa
Dan Belcher <dan@mcneel.com>
2015-09-10 10:26:46 -0700

    Clarify binding ApiDefinition comment

--- ApiDefinition.cs
+++ ApiDefinition.cs
@@ -8,9 +8,9 @@
 
 namespace RhinoMobile
 {
-	// RhinoMobile.Touch.csproj is an Objective-C binding (btouch) project.
-	// Even though we are not binding Objective-C - but rather wrapping
-	// a native C/C++ library (opennurbs) and packaging the RhinoMobile 
-	// classes along with it - an ApiDefinition.cs is required for all 
-	// btouch projects.
+	// RhinoMobile.Touch.csproj and RhinoMobile.iOS are Objective-C 
+	// binding (btouch) projects. Even though we are not binding 
+	// Objective-C - but rather wrapping a native C/C++ library 
+	// (opennurbs) and packaging the RhinoMobile  classes along with 
+	// it - an ApiDefinition.cs is required for all btouch projects.
 }
